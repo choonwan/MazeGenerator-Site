5a4ac60b45c68d1e86b9085e2f460f35e451f4cc
Chong <121586363+choonwan@users.noreply.github.com>
2024-07-18 21:59:58 -0300

    Add files via upload

---
+++ JS/Algorithm.js
@@ -0,0 +1,91 @@
+function randomNumber(max){
+    return Math.floor(Math.random() * max)
+}
+
+function formatMaze(size, number){
+    console.log("begin")
+    algorithm(size, number, [], [], 1);
+    console.log("done")
+}
+
+function algorithm(size, number, visited, intersections, position){
+    visited.push(position)
+    var nextPlace;
+    var possibilities = []
+
+    console.log(position, visited, intersections)
+    //right
+    if (!(visited.filter((place) => place == (position+1)).length) && ((position%size) != 0)){
+        possibilities.push(position+1)
+    }
+    //left
+    if (!(visited.filter((place) => place == (position-1)).length) && (((position-1)%size) != 0)){
+        possibilities.push(position-1)
+    }
+    //bottom
+    if (!(visited.filter((place) => place == (position+size)).length) && ((position+size) <= (size*size))){
+        possibilities.push(position+size)
+    }
+    //top
+    if (!(visited.filter((place) => place == (position-size)).length) && ((position-size) > 0)){
+        possibilities.push(position-size)
+    }
+
+    console.log(possibilities)
+
+    if (possibilities.length > 1){
+        intersections.push(position)
+    }
+    else if (possibilities.length == 0){
+        if (intersections.length == 0){
+            return
+        }
+        else{
+            let index = randomNumber(intersections.length)
+            nextPlace = intersections[index]
+            intersections.splice(index, 1)
+            algorithm(size,number,visited, intersections,nextPlace)
+            return
+        }
+    }
+
+    let index = randomNumber(possibilities.length)
+    nextPlace = possibilities[index]
+    removeBarrier(size,number,position,nextPlace)
+    algorithm(size,number,visited,intersections,nextPlace)
+    return
+}
+
+function removeBarrier(size,number,position, nextPosition){
+    var diference = position - nextPosition
+    var pos = document.getElementById("Maze-col-"+((size*size)*(number-1) + position))
+    var newPos = document.getElementById("Maze-col-"+((size*size)*(number-1) + nextPosition))
+    // left
+    if (diference == 1){
+        pos.classList.add("border-start-0");
+        newPos.classList.add("border-end-0");
+        pos.style.paddingLeft = "1px"
+        newPos.style.paddingRight = "1px"
+    }
+    // right
+    else if (diference == -1){
+        pos.classList.add("border-end-0");
+        newPos.classList.add("border-start-0");
+        pos.style.paddingRight = "1px"
+        newPos.style.paddingLeft = "1px"
+    }
+    // top
+    else if (diference == size){
+        pos.classList.add("border-top-0");
+        newPos.classList.add("border-bottom-0");
+        pos.style.paddingTop = "1px"
+        newPos.style.paddingBottom = "1px"
+    }
+    // bottom
+    else if (diference == -size){
+        pos.classList.add("border-bottom-0");
+        newPos.classList.add("border-top-0");
+        pos.style.paddingBottom = "1px"
+        newPos.style.paddingTop = "1px"
+    }
+}
---
+++ JS/Maze-display.js
@@ -0,0 +1,30 @@
+var mazeCount = 0;
+var colCount = 0;
+var rowCount = 0;
+
+function generateMaze(size){
+    var measurements = 100/size;
+    var parent = document.createElement("div");
+    parent.className = "Maze container";
+    parent.id = "Maze-" + size + "-" + (mazeCount+1);
+    document.body.appendChild(parent)
+    for(let i = 0; i<size; i++){
+        rowCount++
+        var child_row = document.createElement("div");
+        child_row.className = "row Maze-row g-0";
+        child_row.id = "Maze-row-"+ rowCount;
+        child_row.style = "height:"+measurements+"%;"
+        parent.appendChild(child_row)
+        for(let u = 0; u<size; u++){
+            colCount++
+            var child_col = document.createElement("div");
+            child_col.className = "col Maze-col border border-black border-1";
+            child_col.id = "Maze-col-"+ colCount;
+            child_col.style = "width:" + measurements + "%;height: 100%;"
+            child_row.appendChild(child_col);
+        }
+    }
+    mazeCount++
+    const button = document.createElement("button")
+    button.onclick(formatMaze(size,mazeCount))
+}
